aec19abecaf2489a851ad2a298f0bf30fd47f0e9
mh <mh@immerda.ch>
2008-01-16 20:52:59 +0000

    added nfsd

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,36 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/nfs/manifests/init.pp - manage nfs stuff
 # Copyright (C) 2007 admin@immerda.ch
 #
 
-# modules_dir { "skeleton": }
+# modules_dir { "nfs": }
 
-class skeleton {
+class nfs {
+    service{"nfsd": 
+        ensure => running, 
+        require => Service["portmap"],
+    }
+    service{"portmap": 
+        ensure => running, 
+        hasrestart => true,
+    }
+}
 
+define nfs::deploy_config($source){
+    file{"/etc/exports":
+        owner => root,
+        group => 0,
+        mode => 600,
+        source => "puppet://$servername/dist/nfsd/exports/$source",
+        notify => service["nfsd"],
+        require => service["nfsd"],
+    }
+
+    file{"/etc/hosts.allow":
+        owner => root,
+        group => 0,
+        mode => 600, 
+        source => "puppet://$servername/dist/nfsd/hosts.allow/$source",
+        notify => service["portmap"],
+        require => service["portmap"],
+    }
 }
