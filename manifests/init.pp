6369aa73bb4bded1a180326be4622cb35676abde
mh <mh@immerda.ch>
2008-06-13 20:20:39 +0000

    Merge commit 'puzzle/development'

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,15 @@
-# modules/nfsd/manifests/init.pp - manage nfsd stuff
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
-# adpated by  Puzzle ITC - haerry+puppet(at)puzzle.ch
+#
+# apache module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
 #
 # This module manages an nfs server. to mange an nfs 
 # client please look into the module nfs
