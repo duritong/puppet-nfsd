fa00d3d3816a042b80b60d8c18b06c5ad5366bd3
mh <mh@immerda.ch>
2009-11-14 15:08:25 +0100

    update to 0.25.x style

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,7 +1,7 @@
 class nfsd::centos inherits nfsd::base {
   file{'/etc/sysconfig/nfs':
-    source => [ "puppet://$server/files/nfsd/sysconfig/${fqdn}/nfs",
-                "puppet://$server/files/nfsd/sysconfig/nfs",
+    source => [ "puppet://$server/modules/site-nfsd/sysconfig/${fqdn}/nfs",
+                "puppet://$server/modules/site-nfsd/sysconfig/nfs",
                 "puppet://$server/modules/nfsd/sysconfig/nfs" ],
         notify => Service["nfs"],
         owner => root, group => 0, mode => 644;
